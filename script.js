12c94fd6bf77f0886efd99df668963ea6858e564
onomadhe <nomadhe@outlook.com>
2023-03-03 10:05:02 -0300

    remoção de paginas bio

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ chk.addEventListener("change", () => {
     document.body.classList.toggle("dark")
 })
 
+let docTitle = document.title;
+window.addEventListener("blur", () => {
+    document.title = "Volte Logo! :(";
+})
+window.addEventListener("focus", () => {
+    document.title = docTitle;
+})
